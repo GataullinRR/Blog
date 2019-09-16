0dbb8500dd7e32bca5069328658b9c2fc16eeae5
Radmir <blabla@lala.com>
2019-09-16 08:43:09 +0500

    Working on Login and Registration

--- Blog/Pages/Index.cshtml.cs
+++ Blog/Pages/Index.cshtml.cs
@@ -24,5 +24,10 @@ namespace Blog.Pages
         {
 
         }
+
+        public void OnPost()
+        {
+
+        }
     }
 }
---
+++ Blog/Pages/Login.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Blog.Pages
+{
+    public class LoginModel : PageModel
+    {
+        [BindProperty]
+        [Required(ErrorMessage = "Login is required")]
+        public string Login { get; set; }
+        [BindProperty]
+        [Required(ErrorMessage = "Password is required")]
+        public string Password { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnPost()
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return Page();
+            }
+        }
+    }
+}
