950924df7d3a5c62621c921fe78bff56c2b423a1
Radmir <QTU100@gmail.com>
2019-10-20 17:44:37 +0500

    Added profile rule violation list

---
+++ ASPCoreUtilities/Extensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Utilities.Extensions;
+
+namespace ASPCoreUtilities
+{
+    public static class Extensions
+    {
+        public static string GetController(this string controllerNameOf)
+        {
+            return controllerNameOf.Remove("Controller")
+        }
+    }
+}
