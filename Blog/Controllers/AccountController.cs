ff9d645bc9ec1c9003da582759dd09a5ff319dc3
Radmir <QTU100@gmail.com>
2019-12-03 20:10:06 +0500

    close #80 (added AJAX attribute, but not all actions are marked)

--- Blog/Controllers/AccountController.cs
+++ Blog/Controllers/AccountController.cs
@@ -4,6 +4,7 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Blog.Misc;
 using Blog.Models;
 using Blog.Pages.Account;
 using Blog.Services;
--- Blog/Controllers/CommentaryController.cs
+++ Blog/Controllers/CommentaryController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
+using Blog.Misc;
 using Blog.Models;
 using Blog.Services;
 using DBModels;
