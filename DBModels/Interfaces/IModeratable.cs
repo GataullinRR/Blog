4e11169f02b343b63f0bdcb66776cb833f370cf0
Radmir <QTU100@gmail.com>
2019-12-15 19:07:15 +0500

    close #90

--- DBModels/Interfaces/IModeratable.cs
+++ DBModels/Interfaces/IModeratable.cs
@@ -1,9 +1,14 @@
-﻿namespace DBModels
+﻿using System.ComponentModel;
+
+namespace DBModels
 {
     public enum ModerationState
     {
+        [Description("UNDER MODERATION")]
         UNDER_MODERATION = 0,
+        [Description("MODERATION NOT PASSED")]
         MODERATION_NOT_PASSED = 1000,
+        [Description("MODERATED")]
         MODERATED = 2000,
     }
 
